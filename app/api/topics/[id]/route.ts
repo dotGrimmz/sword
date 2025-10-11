36f7a09e9bad4f6a198dc0efb58172766dffff4a
rakeem <rakeemxng@gmail.com>
2025-10-11 06:55:13 -0400

    files added but need to flush out api logic

---
+++ app/api/topics/[id]/route.ts
@@ -0,0 +1,25 @@
+import { supabase } from "@/lib/supabaseClient";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { data, error } = await supabase
+    .from("topics")
+    .select(
+      `
+      *,
+      evidence (*, evidence_scriptures(*)),
+      counters (*, counter_sources(*)),
+      topic_sources (*, sources(*)),
+      topic_scriptures (*, scriptures(*))
+    `
+    )
+    .eq("id", params.id)
+    .single();
+
+  if (error)
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  return NextResponse.json(data);
+}
